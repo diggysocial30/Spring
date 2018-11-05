72afadd62fab31849a3e1b81e4d0adda4e449ae1
divya <dgupta26@sapient.com>
2018-11-05 16:36:54 +0530

    Removed spring boot dependency and moved to annotations

--- demo/src/main/java/com/DemoApplication.java
+++ demo/src/main/java/com/DemoApplication.java
@@ -1,65 +1,45 @@
 package com;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-import com.cdi.SomeCDIBusiness;
 import com.diggysocial30.springbasics.demo.BinarySearchImpl;
-import com.jdbc.PersonDAO;
 
-import scan.SDAO;
-
-
-/*SpringBootApplication scans for the cpmponents and the dependencies
+/*Removed the spring boot configurations
 */
-
-@SpringBootApplication
-@ComponentScan({"scan","com.diggysocial30.springbasics.demo","com.jdbc","com.cdi"})
+@Configuration
+@ComponentScan
 public class DemoApplication {
-	
-	private static Logger LOGGER=LoggerFactory.getLogger(DemoApplication.class);
 
-	/*what are beans ?
-	what are the dependency of a bean ?
-	where to search for the beans ?		*/
-	
+	/*
+	 * what are beans ? what are the dependency of a bean ? where to search for
+	 * the beans ?
+	 */
+
+	private static Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-		
-		// Spring Applicaton context will manage all the beans
-		
-		BinarySearchImpl binarySearch = context.getBean(BinarySearchImpl.class);
-		BinarySearchImpl binarySearch1 = context.getBean(BinarySearchImpl.class);
-		
-		// Default scope of a bean: singleton
-		// Same bean is returned each time
-		//binarySearch== binarySearch1;
-		System.out.println(binarySearch);
-		System.out.println(binarySearch1);
-		int result=binarySearch.binarySearch(new int[]{1,2,4}, 4);
-		System.out.println(result);
-		
-		
-		PersonDAO personDAO= context.getBean(PersonDAO.class);
-		
-		PersonDAO personDAO2= context.getBean(PersonDAO.class);
-		SDAO sDAO2= context.getBean(SDAO.class);
-		LOGGER.info("{}",personDAO);
-		LOGGER.info("{}",personDAO2);
-		
+		try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(DemoApplication.class)) {
 
-		LOGGER.info("{}",personDAO.getJdbcConnection());
-		LOGGER.info("{}",personDAO2.getJdbcConnection());
-		
-		SomeCDIBusiness someCDIBusiness= context.getBean(SomeCDIBusiness.class);
-		
-		LOGGER.info("{}",someCDIBusiness);
-		LOGGER.info("{}",someCDIBusiness.getDao());
-		
+			// Spring Applicaton context will manage all the beans
+
+			BinarySearchImpl binarySearch = context.getBean(BinarySearchImpl.class);
+			BinarySearchImpl binarySearch1 = context.getBean(BinarySearchImpl.class);
+
+			// Default scope of a bean: singleton
+			// Same bean is returned each time
+			// binarySearch== binarySearch1;
+			System.out.println(binarySearch);
+			System.out.println(binarySearch1);
+			int result = binarySearch.binarySearch(new int[] { 1, 2, 4 }, 4);
+			System.out.println(result);
+			
+			LOGGER.info("**********YAYYY******************");
+		}
+
 	}
 }
