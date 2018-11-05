c90857a9ca974fb943b2961e0b6039c04ce5fbc5
divya <dgupta26@sapient.com>
2018-11-05 13:08:09 +0530

    Scope of beans

--- demo/src/main/java/com/diggysocial30/springbasics/demo/BinarySearchImpl.java
+++ demo/src/main/java/com/diggysocial30/springbasics/demo/BinarySearchImpl.java
@@ -2,9 +2,27 @@ package com.diggysocial30.springbasics.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+
+/*5 types of bean scopes are supported :
+
+Singleton : It returns a single bean instance per Spring IoC container.This single instance is stored in a cache of such singleton beans, and all subsequent requests and references for that named bean return the cached object.If no bean scope is specified in bean configuration file, default to singleton.
+
+Prototype : It returns a new bean instance each time when requested. It does not store any cache version like singleton.
+
+Request : It returns a single bean instance per HTTP request.
+
+Session : It returns a single bean instance per HTTP session (User level session).
+
+GlobalSession : It returns a single bean instance per global HTTP session. It is only valid in the context of a web-aware Spring ApplicationContext (Application level session).
+
+In most cases, you may only deal with the Spring’s core scope – singleton and prototype, and the default scope is singleton.*/
+
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl { 
 	
 	@Autowired
--- demo/src/main/java/com/diggysocial30/springbasics/demo/DemoApplication.java
+++ demo/src/main/java/com/diggysocial30/springbasics/demo/DemoApplication.java
@@ -21,6 +21,13 @@ public class DemoApplication {
 		// Spring Applicaion context will manage all the beans
 		
 		BinarySearchImpl binarySearch = context.getBean(BinarySearchImpl.class);
+		BinarySearchImpl binarySearch1 = context.getBean(BinarySearchImpl.class);
+		
+		// Default scope of a bean: singleton
+		// Same bean is returned each time
+		//binarySearch== binarySearch1;
+		System.out.println(binarySearch);
+		System.out.println(binarySearch1);
 		int result=binarySearch.binarySearch(new int[]{1,2,4}, 4);
 		System.out.println(result);
 	}
