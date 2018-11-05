6dfcc4a888656036fcb40a6959c888bc3454fee1
divya <dgupta26@sapient.com>
2018-11-05 11:59:50 +0530

    Added annotation | Primary

--- demo/src/main/java/com/diggysocial30/springbasics/demo/QuickSortAlgorithm.java
+++ demo/src/main/java/com/diggysocial30/springbasics/demo/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.diggysocial30.springbasics.demo;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 
 	@Override
