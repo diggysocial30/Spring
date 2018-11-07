18556345a595ab3f3f814ca06999d40bf70989ae
divya <dgupta26@sapient.com>
2018-11-07 12:12:15 +0530

    Mixing xml and component scan

--- demo/src/main/java/com/DemoApplicationXML.java
+++ demo/src/main/java/com/DemoApplicationXML.java
@@ -2,7 +2,6 @@ package com;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.jdbc.xml.XMLJDBCConnection;
@@ -10,10 +9,9 @@ import com.jdbc.xml.XMLPersonDAO;
 
 /*Removed the spring boot configurations
 */
-@Configuration
 public class DemoApplicationXML {
 
-	/*
+	/*R
 	 * what are beans ? what are the dependency of a bean ? where to search for
 	 * the beans ?
 	 */
@@ -22,7 +20,9 @@ public class DemoApplicationXML {
 
 	public static void main(String[] args) {
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
-
+             
+			
+			LOGGER.info("Beans loaded => {}",(Object)context.getBeanDefinitionNames());
 			XMLPersonDAO xmlPersonDAO=context.getBean(XMLPersonDAO.class);
 			XMLJDBCConnection xmlJDBC=xmlPersonDAO.getJdbcConnection();
 
